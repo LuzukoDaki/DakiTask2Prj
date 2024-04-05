d80ca4f83f0278b5639132aa239c7bf1679b4954
LuzukoDaki <lzkdaki@gmail.com>
2024-04-05 09:42:20 +0000

    First Commit

---
+++ src/dakilearnertask2prj/DakiLearnerTask2Prj.java
@@ -0,0 +1,14 @@
+
+package dakilearnertask2prj;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class DakiLearnerTask2Prj {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
